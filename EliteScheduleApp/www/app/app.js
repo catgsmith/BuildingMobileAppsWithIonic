6f1f5060f713691a740f1248f13c8e258acebe07
catgsmith <catgsmith@hotmail.com>
2016-02-24 21:14:53 +0000

    initial setup

---
+++ EliteScheduleApp/www/app/app.js
@@ -0,0 +1,27 @@
+angular.module('eliteApp', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('home', {
+      abstract: true,
+      url: "/home",
+      templateUrl: "app/home/home.html"
+    });
+    // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/teams');
+});
