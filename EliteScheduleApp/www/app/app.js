d5b1e2c19c8c6aa05b5279e44510efe6a74b88ed
catgsmith <catgsmith@hotmail.com>
2016-02-24 21:57:23 +0000

    03-Setup SideMenu

--- EliteScheduleApp/www/app/app.js
+++ EliteScheduleApp/www/app/app.js
@@ -20,7 +20,11 @@ angular.module('eliteApp', ['ionic'])
     .state('home', {
       url: "/home",
       templateUrl: "app/home/home.html"
+    })
+    .state('app', {
+      url: "/app",
+      templateUrl: "app/layout/menu-layout.html"
     });
     // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/app');
 });
