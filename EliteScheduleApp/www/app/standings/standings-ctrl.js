c873848ffff4eb407c88b01baf7a3b878bda3a93
catgsmith <catgsmith@hotmail.com>
2016-03-04 22:23:20 +0000

    12-Add standing list with list dividers (wins - losses)

---
+++ EliteScheduleApp/www/app/standings/standings-ctrl.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('eliteApp').controller('StandingsCtrl', ['eliteApi', StandingsCtrl]);
+
+    function StandingsCtrl(eliteApi) {
+        var vm = this;
+        
+        var data = eliteApi.getLeagueData();
+
+        vm.standings = data.standings;
+
+    }
+})();
