2439e1b42e738f2593b23bc1cf2dc1649901692e
catgsmith <catgsmith@hotmail.com>
2016-03-04 21:52:25 +0000

    11-Add list to app/locations

---
+++ EliteScheduleApp/www/app/locations/locations-ctrl.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('eliteApp').controller('LocationsCtrl', ['eliteApi', LocationsCtrl]);
+
+    function LocationsCtrl(eliteApi) {
+        var vm = this;
+        
+        var data = eliteApi.getLeagueData();
+        vm.locations = data.locations;
+
+    }
+})();
