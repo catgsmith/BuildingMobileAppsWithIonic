48f6392511277061eb1605218bdb0b4ed976edda
catgsmith <catgsmith@hotmail.com>
2016-04-13 12:52:14 +0100

    Drop marker on Google map location

--- EliteScheduleApp/www/app/locations/location-map-ctrl.js
+++ EliteScheduleApp/www/app/locations/location-map-ctrl.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
     'use strict';
 
-    angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', LocationMapCtrl]);
+    angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', 'eliteApi', LocationMapCtrl]);
 
-    function LocationMapCtrl($stateParams) {
+    function LocationMapCtrl($stateParams, eliteApi) {
         var vm = this;
-        
+
         vm.locationId = Number($stateParams.id);
 
         vm.map = {
@@ -15,6 +15,25 @@
             },
             zoom: 12
         };
-        vm.marker = { };
+        vm.marker = {};
+
+        eliteApi.getLeagueData().then(function(data) {
+
+            vm.location = _.find(data.locations, { id: vm.locationId });
+            vm.marker = {
+                latitude: vm.location.latitude,
+                longitude: vm.location.longitude,
+                title: vm.location.name + "<br/>(Tap for directions)",
+                showWindow: true
+            };
+
+            vm.map.center.latitude = vm.location.latitude;
+            vm.map.center.longitude = vm.location.longitude;
+        });
+
+        vm.locationClicked = function(marker) {
+            window.location = "geo:" + marker.latitude + "," + marker.longitude + ";u=35";
+        };
+
     }
 })();
