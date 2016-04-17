73bf7e42a1e230449a6e167e99bf69e2560c31ad
catgsmith <catgsmith@hotmail.com>
2016-04-17 21:58:16 +0100

    installation and setup for ngCordova

---
+++ ToastScanPictures/www/app/dialogs/dialogs-ctrl.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular.module('ngApp').controller('DialogsCtrl', ['$scope', '$cordovaDialogs', DialogsCtrl]);
+
+    function DialogsCtrl($scope, $cordovaDialogs){
+
+    }
+})();
---
+++ ToastScanPictures/www/js/scan.js
@@ -0,0 +1,29 @@
+var scan = function() {
+	   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled + "\n");           
+			alert(fields);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+};
+
+var getPics = function() {
+	window.imagePicker.getPictures(
+		function(results) {
+			for (var i = 0; i < results.length; i++) {
+				console.log('Image URI: ' + results[i]);
+			}
+		}, function (error) {
+			console.log('Error: ' + error);
+		}, {
+			maximumImagesCount: 10,
+			width: 800
+		}
+	);
+};
